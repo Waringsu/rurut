987a5b63789e0ca622c974f7350a1302b708674d
babehdyo <widyo.babeh@gmail.com>
2021-05-24 20:32:16 +0700

    Update digi.sh

--- digi.sh
+++ digi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
+apt update
+apt install wget unzip -y 
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u DFtFJRa7oUzbBpUxYLXFaeTVDwyidT1TMk -p c=DGB,zap=PYRK-lyra2z330 -q
