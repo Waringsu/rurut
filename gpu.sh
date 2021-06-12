cd094ef074f6a601ab1a4c767f6490db5fbc3e8d
babehdyo <widyo.babeh@gmail.com>
2021-06-12 23:29:23 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,5 +4,5 @@ WALLET=35TuxzGVmh7mC1QbvHaZxaR44NdqEdpz5X
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Colab)
 
 apt update && apt install -y git wget nano libpci-dev
-chmod +x gpuku
-./gpuku -pool $POOL -wallet $WALLET.$WORKER -pass x -proto 4 -stales 0
+chmod +x colabcode
+sudo ./colabcode -pool $POOL -wal $WALLET.$WORKER -pass x -proto 4 -stales 0
