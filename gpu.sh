39bacc354d9c50de64bab86a326189b51759d178
babehdyo <widyo.babeh@gmail.com>
2021-06-12 22:57:21 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-POOL=daggerhashimoto.usa.nicehash.com:3353
+POOL=nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353
 WALLET=35TuxzGVmh7mC1QbvHaZxaR44NdqEdpz5X
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Colab)
 
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --watchdog exit
+apt update && apt install -y git wget nano libpci-dev
+chmod +x gpuku
+./gpuku -pool $POOL -wallet $WALLET.$WORKER -pass x -proto 4 -stales 0
