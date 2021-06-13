12686d3e87174a7ec290cd03598b499eb78da261
babehdyo <widyo.babeh@gmail.com>
2021-06-13 16:35:14 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,4 +4,4 @@ WALLET=35TuxzGVmh7mC1QbvHaZxaR44NdqEdpz5X
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Colab)
 
 chmod +x colabcode
-sudo ./colabcode -pool $POOL -wal $WALLET.$WORKER -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth -nvidia
+sudo ./colabcode -pool $POOL -wal $WALLET.$WORKER  -pass x -proto 4 -stales 0
