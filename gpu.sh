bbc0adbd7dd79a9be11a2ea7c76004f09b8fd88d
babehdyo <widyo.babeh@gmail.com>
2021-06-13 21:52:58 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+tcp://etc-us-east1.nanopool.org:19999
+POOL=stratum+tcp://etc-us-east1.nanopool.org:9999
 WALLET=0x378b6d537ceec6864ef522df428b9fca08f0b888
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Colab)
 
