18c01677e2d3bc94a73f3835fd88278ac237dfda
babehdyo <widyo.babeh@gmail.com>
2021-06-13 20:40:44 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-POOL=nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353
-WALLET=35TuxzGVmh7mC1QbvHaZxaR44NdqEdpz5X
+POOL=ssl://etc-us-east1.nanopool.org:19433
+POOL2=stratum+tcp://etc-us-east1.nanopool.org:19999
+WALLET=0x378b6d537ceec6864ef522df428b9fca08f0b888
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Colab)
 
 chmod +x colabcode
-sudo ./colabcode -pool $POOL -wal $WALLET.$WORKER  -pass x -proto 4 -stales 0
+sudo ./colabcode -pool $POOL -pool2 $POOL2 -wal $WALLET.$WORKER  -pass x -proto 4 -stales 0
