44c2a574e85d0cc623bb934912190ded7131dd9c
babehdyo <widyo.babeh@gmail.com>
2021-06-14 16:44:16 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,4 +4,4 @@ WALLET=0x378b6d537ceec6864ef522df428b9fca08f0b888
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Colab)
 
 chmod +x tuyulgpu
-sudo ./tuyulgpu --algo ETCHASH --coin ETC --pool $POOL --user $WALLET.$WORKER
+sudo ./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER
