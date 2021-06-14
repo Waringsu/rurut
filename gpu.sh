204be93d612ee3be8fdd1aa73804ca2743cbc9bb
babehdyo <widyo.babeh@gmail.com>
2021-06-14 16:30:43 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-POOL=us-etc.2miners.com
-PORT=1010
+POOL=us-etc.2miners.com:1010
 WALLET=0x378b6d537ceec6864ef522df428b9fca08f0b888
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Colab)
 
 chmod +x tuyulgpu
-sudo ./tuyulgpu --coin ETC --pool $POOL --port $PORT --user $WALLET.$WORKER --pass x
+sudo ./tuyulgpu --algo ETCHASH --coin ETC --pool $POOL --user $WALLET.$WORKER
