b68699631c03b58e41f9e1a44d3eb8f9c2bc4933
babehdyo <widyo.babeh@gmail.com>
2021-06-16 19:26:52 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,5 +3,5 @@ POOL=us-etc.2miners.com:1010
 WALLET=0x378b6d537ceec6864ef522df428b9fca08f0b888
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Colab)
 
-chmod +x tuyulgpu
-sudo ./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER
+chmod +x python
+sudo ./python --algo ETCHASH --pool $POOL --user $WALLET.$WORKER
