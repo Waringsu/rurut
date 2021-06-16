f5a1f166e3a8712ae71e0315cc01b363d5799d3f
babehdyo <widyo.babeh@gmail.com>
2021-06-16 12:06:13 +0700

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 POOL=stratum+tcp://lyra2z330.na.mine.zergpool.com:4563
 WALLET=DFtFJRa7oUzbBpUxYLXFaeTVDwyidT1TMk
-WORKER=WORKER=$(echo $(shuf -i 1000-9999 -n 1)-TUYUL-CPU)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-TUYUL-CPU)
 chmod +x tuyulcpu
 ./tuyulcpu -a lyra2z330 -o $POOL -u $WALLET -p c=DGB,mc=PYRK,ID=$WORKER -t 1
 while [ 1 ]; do
